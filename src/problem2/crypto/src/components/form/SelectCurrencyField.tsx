4a67010d1045db7402f0f4ab13a2e0916a12c550
Bk49 <ericngyongwei@gmail.com>
2024-02-25 16:38:52 +0800

    Beautify the drop down options

--- src/problem2/crypto/src/components/form/SelectCurrencyField.tsx
+++ src/problem2/crypto/src/components/form/SelectCurrencyField.tsx
@@ -2,6 +2,7 @@ import {
     FormControl,
     FormControlProps,
     FormHelperText,
+    Grid,
     InputLabel,
     MenuItem,
     Select,
@@ -14,25 +15,25 @@ import {
     RegisterOptions,
     useFormContext,
 } from "react-hook-form";
+import { tokenChoices } from "../../constants/token_choices";
 
 export interface DropdownChoices {
     text: string | number;
     value: string | number;
+    img?: string;
     id?: string | number;
 }
 
-interface SelectFieldCurrencyFieldProps extends FormControlProps {
+interface SelectCurrencyFieldProps extends FormControlProps {
     name: string;
     label: string;
-    choices: Array<DropdownChoices>;
     rules?: RegisterOptions<FieldValues, string>;
 }
 
-const SelectFieldCurrencyField: React.FC<SelectFieldCurrencyFieldProps> = ({
+const SelectCurrencyField: React.FC<SelectCurrencyFieldProps> = ({
     name,
     variant,
     label,
-    choices,
     rules,
     ...props
 }) => {
@@ -57,18 +58,52 @@ const SelectFieldCurrencyField: React.FC<SelectFieldCurrencyFieldProps> = ({
                         label={label}
                         {...field}
                         renderValue={(selected) => (
-                            <Typography variant="subtitle1">
-                                {
-                                    choices.find(
-                                        (choice) => choice.value === selected
-                                    )?.text
-                                }
-                            </Typography>
+                            <Grid direction="row" container>
+                                {selected.text !== "" && (
+                                    <Grid item xs={1}>
+                                        <img
+                                            src={
+                                                tokenChoices.find(
+                                                    (choice) =>
+                                                        choice.value ===
+                                                        selected
+                                                )?.img
+                                            }
+                                        />
+                                    </Grid>
+                                )}
+                                <Grid item>
+                                    <Typography variant="subtitle1">
+                                        {
+                                            tokenChoices.find(
+                                                (choice) =>
+                                                    choice.value === selected
+                                            )?.text
+                                        }
+                                    </Typography>
+                                </Grid>
+                            </Grid>
                         )}
                     >
-                        {choices.map(({ text, value }) => (
+                        {tokenChoices.map(({ text, value, img }) => (
                             <MenuItem key={value} value={value}>
-                                {value === "" ? <em>{text}</em> : text}
+                                <Grid direction="row" container>
+                                    {!!img && (
+                                        <Grid item xs={1}>
+                                            <img src={img} />
+                                        </Grid>
+                                    )}
+                                    <Grid item>
+                                        <Typography>
+                                            {" "}
+                                            {value === "" ? (
+                                                <em>{text}</em>
+                                            ) : (
+                                                text
+                                            )}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
                             </MenuItem>
                         ))}
                     </Select>
@@ -81,4 +116,4 @@ const SelectFieldCurrencyField: React.FC<SelectFieldCurrencyFieldProps> = ({
     );
 };
 
-export default SelectFieldCurrencyField;
+export default SelectCurrencyField;

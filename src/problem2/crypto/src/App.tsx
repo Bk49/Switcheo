0f8096e78ad0fe533eb1312399e31cc55300e304
Bk49 <ericngyongwei@gmail.com>
2024-02-25 16:39:41 +0800

    Refactor token choices

--- src/problem2/crypto/src/App.tsx
+++ src/problem2/crypto/src/App.tsx
@@ -1,33 +1,132 @@
 import { FormProvider, useForm } from "react-hook-form";
 import Heading from "./components/text/Heading";
 import TextField from "./components/form/TextField";
-import { Container } from "@mui/material";
+import { Container, Grid, Paper, Typography } from "@mui/material";
+import SelectCurrencyField from "./components/form/SelectCurrencyField";
+import { getCryptoPrice } from "./api/token_exchange";
+import { useEffect, useState } from "react";
+import { enqueueSnackbar } from "notistack";
 
 const App = () => {
     const formState = useForm();
-    const { handleSubmit } = formState;
+    const { watch, getValues } = formState;
+    const watching = watch(["input_currency", "output_currency", "amount"]);
+    const [input_currency, output_currency, amount] = getValues([
+        "input_currency",
+        "output_currency",
+        "amount",
+    ]);
+    const [price, setPrice] = useState(0);
+
+    useEffect(() => {
+        if (!!input_currency && !!output_currency && amount > 0) {
+            (async () => {
+                try {
+                    const priceData = await getCryptoPrice(
+                        input_currency,
+                        output_currency,
+                        amount
+                    );
+                    if (!priceData) {
+                        throw new Error();
+                    }
+                    setPrice(priceData);
+                } catch (e) {
+                    enqueueSnackbar("There is an error swapping the currency", {
+                        variant: "error",
+                    });
+                }
+            })();
+        } else {
+            setPrice(0);
+        }
+    }, [watching]);
 
     return (
-        <>
-            <Container>
-                <Heading color="primary">Currency Swap</Heading>
+        <Container>
+            <Grid
+                container
+                height="100%"
+                alignItems="center"
+                justifyContent="center"
+                direction="row"
+                my="10%"
+            >
+                <Paper sx={{ maxWidth: "600px", padding: "2rem" }}>
+                    <Heading color="primary">Currency Swap</Heading>
 
-                <FormProvider {...formState}>
-                    <TextField
-                        name="amount"
-                        type="number"
-                        label="From"
-                        rules={{
-                            required: "Please input amount of currency to swap",
-                            min: {
-                                value: 0,
-                                message: "Amount cannot be negative",
-                            },
-                        }}
-                    />
-                </FormProvider>
-            </Container>
-        </>
+                    <Grid container mt="1rem" direction="column" spacing={2}>
+                        <Grid item>
+                            <FormProvider {...formState}>
+                                <Grid
+                                    container
+                                    direction="column"
+                                    mx="1rem"
+                                    gap={2}
+                                >
+                                    <Grid item>
+                                        <SelectCurrencyField
+                                            name="input_currency"
+                                            label="You Pay"
+                                            variant="filled"
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <SelectCurrencyField
+                                            name="output_currency"
+                                            label="You Receive"
+                                            variant="filled"
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <TextField
+                                            fullWidth
+                                            variant="filled"
+                                            name="amount"
+                                            type="number"
+                                            label="Amount to Swap"
+                                            rules={{
+                                                required:
+                                                    "Please input amount of currency to swap",
+                                                min: {
+                                                    value: 0,
+                                                    message:
+                                                        "Amount cannot be negative",
+                                                },
+                                            }}
+                                        />
+                                    </Grid>
+                                </Grid>
+                            </FormProvider>
+                        </Grid>
+                        {/* <Grid item>
+                                <Button
+                                    onClick={handleSubmit(
+                                        async (res) =>
+                                            console.log(
+                                               await getCryptoPrice(
+                                                    res.input_currency,
+                                                    res.output_currency,
+                                                    res.amount
+                                                )
+                                            ),
+                                        (e) => console.log(e)
+                                    )}
+                                    color="primary"
+                                    variant="contained"
+                                >
+                                    Convert!
+                                </Button>
+                            </Grid> */}
+                        <Grid item>
+                            <Typography>{`Amount you receive: ${
+                                price === 0 ? "N/A" : price
+                            }`}</Typography>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+        </Container>
     );
 };
 
--- src/problem2/crypto/src/constants/token_choices.ts
+++ src/problem2/crypto/src/constants/token_choices.ts
@@ -1,15 +1,29 @@
-export const tokenChoices = [
+import { DropdownChoices } from "../components/form/SelectCurrencyField";
+import ADASvg from "../assets/tokens/ADA.svg";
+import BCHSvg from "../assets/tokens/BCH.svg";
+import BNBSvg from "../assets/tokens/BNB.svg";
+import BTCSvg from "../assets/tokens/BTC.svg";
+import DOGESvg from "../assets/tokens/DOGE.svg";
+import DOTSvg from "../assets/tokens/DOT.svg";
+import ETHSvg from "../assets/tokens/ETH.svg";
+import LINKSvg from "../assets/tokens/LINK.svg";
+import LTCSvg from "../assets/tokens/LTC.svg";
+import USDTSvg from "../assets/tokens/USDT.svg";
+import XLMSvg from "../assets/tokens/XLM.svg";
+import XRPSvg from "../assets/tokens/XRP.svg";
+
+export const tokenChoices: DropdownChoices[] = [
     { id: 0, value: "", text: "Please Select a currency to begin swapping" },
-    { id: 1, value: "ADA", text: "ADA" },
-    { id: 2, value: "BCH", text: "BCH" },
-    { id: 3, value: "BNB", text: "BNB" },
-    { id: 4, value: "BTC", text: "BTC" },
-    { id: 5, value: "DOGE", text: "DOGE" },
-    { id: 6, value: "DOT", text: "DOT" },
-    { id: 7, value: "ETH", text: "ETH" },
-    { id: 8, value: "LINK", text: "LINK" },
-    { id: 9, value: "LTC", text: "LTC" },
-    { id: 10, value: "USDT", text: "USDT" },
-    { id: 11, value: "XLM", text: "XLM" },
-    { id: 12, value: "XRP", text: "XRP" },
+    { id: 1, value: "ADA", text: "ADA", img: ADASvg },
+    { id: 2, value: "BCH", text: "BCH", img: BCHSvg },
+    { id: 3, value: "BNB", text: "BNB", img: BNBSvg },
+    { id: 4, value: "BTC", text: "BTC", img: BTCSvg },
+    { id: 5, value: "DOGE", text: "DOGE", img: DOGESvg },
+    { id: 6, value: "DOT", text: "DOT", img: DOTSvg },
+    { id: 7, value: "ETH", text: "ETH", img: ETHSvg },
+    { id: 8, value: "LINK", text: "LINK", img: LINKSvg },
+    { id: 9, value: "LTC", text: "LTC", img: LTCSvg },
+    { id: 10, value: "USDT", text: "USDT", img: USDTSvg },
+    { id: 11, value: "XLM", text: "XLM", img: XLMSvg },
+    { id: 12, value: "XRP", text: "XRP", img: XRPSvg },
 ];
